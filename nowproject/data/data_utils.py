Correct x and y coordinates of data, correct loading of static topographic features and fix benchmark computation

nowproject/data/data_utils.py
@@ -1,15 +1,59 @@
 import pathlib
 import xarray as xr
+import numpy as np
 
-def load_static_topo_data(topo_data_path: pathlib.Path, data_dynamic: xr.Dataset):
-    dem = xr.open_rasterio(topo_data_path / "srtm_Switzerland_EPSG21781.tif")
+from nowproject.data.data_config import BOTTOM_LEFT_COORDINATES
+
+
+def prepare_data_dynamic(data_dynamic_path: pathlib.Path):
+    data_dynamic = xr.open_zarr(data_dynamic_path)
+    rzc_shape = data_dynamic.isel(time=0).precip.data.shape
+    x = np.arange(BOTTOM_LEFT_COORDINATES[0], BOTTOM_LEFT_COORDINATES[0] + rzc_shape[1]) + 0.5
+    y = np.arange(BOTTOM_LEFT_COORDINATES[1] + rzc_shape[0] - 1, BOTTOM_LEFT_COORDINATES[1] - 1, -1) + 0.5
+    data_dynamic = data_dynamic.assign_coords({"x": x, "y": y})
+    data_dynamic = data_dynamic.reset_coords(
+        ["radar_names", "radar_quality", "radar_availability"], 
+        drop=True
+        )
+    data_dynamic = data_dynamic.sel(time=slice(None, "2021-09-01T00:00"))
+    data_dynamic = data_dynamic.sel({
+        "x": slice(485, 831), 
+        "y": slice(301, 75)
+    })
+    data_dynamic = data_dynamic.rename({"precip": "feature"})[["feature"]]
+
+    return data_dynamic
+
+
+def load_static_topo_data(topo_data_path: pathlib.Path, data_dynamic: xr.Dataset, 
+                          upsample: bool = True, upsample_factor: int = 13) -> xr.Dataset:
+    """Loads topographic data and resamples it to match the dynamic data loaded for training.
+
+    Parameters
+    ----------
+    topo_data_path : pathlib.Path
+        Path to topographic data file
+    data_dynamic : xr.Dataset
+        Dynamic data used for training
+    upsample : bool
+        If true, upsample the topographic data by the upsample_factor
+    upsample_factor : int
+        Upsampling factor
+
+    Returns
+    -------
+    xr.Dataset
+        Static topographic data with dimensions corresponding to the dynamic data
+    """
+    dem = xr.open_rasterio(topo_data_path)
     dem = dem.isel(band=0, drop=True)
-    dem = dem.rename({"x": "y", "y": "x"})
-    new_y = [y*1000 for y in data_dynamic.y.values[::-1]]
-    new_x = [x*1000 for x in data_dynamic.x.values]
-    dem = dem.interp(coords={"x": new_x, "y": new_y})
+
+    if upsample:
+        dem = dem.coarsen({"x": upsample_factor, "y": upsample_factor}, boundary="trim").mean()
+
     dem["x"] = dem["x"] / 1000
     dem["y"] = dem["y"] / 1000
-    dem = dem.reindex(y=list(reversed(dem.y))).transpose("y", "x")
+    dem = dem.interp(coords={"x": data_dynamic.x.values, "y": data_dynamic.y.values})
+    
 
-    return dem
+    return dem.to_dataset(name="feature")

nowproject/data/rzc_to_zarr.py
@@ -80,8 +80,14 @@ def read_rzc_file(input_path: pathlib.Path,
     if metranet_header is None:
         metranet_header = {}
 
-    x = np.arange(BOTTOM_LEFT_COORDINATES[1], BOTTOM_LEFT_COORDINATES[1] + rzc.shape[1])
-    y = np.arange(BOTTOM_LEFT_COORDINATES[0] + rzc.shape[0] - 1, BOTTOM_LEFT_COORDINATES[0] - 1, -1)
+    
+    x = np.arange(BOTTOM_LEFT_COORDINATES[0], BOTTOM_LEFT_COORDINATES[0] + rzc.shape[1])    
+    y = np.arange(BOTTOM_LEFT_COORDINATES[1] + rzc.shape[0] - 1, BOTTOM_LEFT_COORDINATES[1] - 1, -1)
+
+    # Here we define the pixel centroid coordinate
+    x = x + 0.5
+    y = y + 0.5
+    
     time = rzc_filename_to_time(input_path.as_posix().split("/")[-1])
     radar_availability = input_path.as_posix().split("/")[-1][12:14]
 

nowproject/dev/dev_train.py
@@ -40,16 +40,17 @@ from nowproject.utils.config import (
 from xverif import xverif
 
 # Project specific functions
+from nowproject.data.data_utils import prepare_data_dynamic
 import nowproject.architectures as dl_architectures
 from nowproject.loss import WeightedMSELoss, reshape_tensors_4_loss
 from nowproject.training import AutoregressiveTraining
-from nowproject.utils.scalers import RainScaler
+from nowproject.utils.scalers import RainBinScaler, RainScaler
+from nowproject.utils.plot import plot_averaged_skill, plot_averaged_skills, plot_skills_distribution
+
+model_dir = Path("/home/haddad/experiments/RNN-AR6-UNet-AvgPooling/")
 
 # %load_ext autoreload
 # %autoreload 2
-
-
-model_dir = Path("/home/haddad/experiments/RNN-AR6-UNet-AvgPooling/")
 
 
 # zarr_dir_path = Path("/ltenas3/0_Data/NowProject/zarr/")
@@ -62,34 +63,26 @@ model_dir = Path("/home/haddad/experiments/RNN-AR6-UNet-AvgPooling/")
 
 data_dir_path = Path("/ltenas3/0_Data/NowProject/")
 
-data_dynamic = xr.open_zarr(data_dir_path / "zarr" / "rzc_temporal_chunk.zarr")
-data_dynamic = data_dynamic.reset_coords(
-    ["radar_names", "radar_quality", "radar_availability"], 
-    drop=True
-    )
-data_dynamic = data_dynamic.sel(time=slice(None, "2021-09-01T00:00"))
-# data_dynamic = data_dynamic.sel({"y": list(range(850, 450, -1)), "x": list(range(30, 320))})
-data_dynamic = data_dynamic.sel(
-    {"y": list(range(835, 470, -1)), 
-    "x": list(range(60, 300))}
-    )
-data_dynamic = data_dynamic.rename({"precip": "feature"})[["feature"]]
+data_dynamic = prepare_data_dynamic(data_dir_path / "zarr" / "rzc_temporal_chunk.zarr")
 
 test = data_dynamic.isel(time=slice(0, 100))
 scaler = RainScaler(feature_min=np.log10(0.025), feature_max=np.log10(100), threshold=np.log10(0.1))
 
 
-test_bins = data_dynamic.isel(time=slice(0, 100)).copy()
+test_bins = data_dynamic.isel(time=slice(1300, 1800)).copy()
 np_bins = np.ones(test_bins.feature.shape)
+
 np_bins[:10, 150:250, 200:300] = 3
 np_bins[20:30, 150:250, 200:300] = 9
 np_bins[40:60, 150:250, 200:300] = 14
+np_bins[80:90, 150:250, 200:300] = np.nan
+np_bins[90:100, 150:250, 200:300] = 500
+test_bins["feature"] = (test_bins.feature.dims, np_bins)
 
-bins = [0.01, 0.5, 1.0, 2.0, 4.0, 8.0, 12.0, 20.0, 30.0, 50.0, 80.0, 120.0, 200.0]
+bins = [0.0, 0.1, 0.5, 1.0, 2.0, 4.0, 8.0, 12.0, 20.0, 30.0, 50.0, 80.0, 120.0, 250.0]
 # bins = np.logspace(start=np.log10(0.001), stop=np.log10(200), num=10)
 centres = [0] + [(bins[i] + bins[i+1])/2 for i in range(0, len(bins)-1)] + [np.nan]
-
-
+scaler = RainBinScaler(bins, centres)
 
 test_transformed = scaler.transform(test, variable_dim="feature")
 test_untransformed = scaler.inverse_transform(test_transformed, variable_dim="feature")
@@ -125,6 +118,13 @@ verification_zarr_fpath = (
     model_dir / "model_predictions" / "space_chunked" / "test_forecasts_2.zarr"
 ).as_posix()
 
+rechunked_zarr_fpath = (
+    model_dir / "model_predictions" / "space_chunked" / "rechunked_store.zarr"
+)
+
+ds_rechunked = xr.open_zarr(rechunked_zarr_fpath, chunks="auto")
+
+
 ds_forecasts = ds_forecasts.drop_dims("time")
 
 # Check the chunk size of coords. If chunk size > coord shape, chunk size = coord shape.
@@ -156,6 +156,10 @@ ds_verification_format = xr.open_zarr(verification_zarr_fpath).load()
 
 selection = data_dynamic.sel(time=ds_verification_format.time).load()
 
+
+####################################################
+# Skills
+
 ds_skill = xverif.deterministic(
         pred=ds_verification_format.chunk({"x": 1, "y": 1}),
         obs=selection.chunk({"x": 1, "y": 1}),
@@ -168,3 +172,14 @@ ds_skill.to_netcdf((model_dir / "model_skills" / "deterministic_spatial_skill.nc
 ds_skill = xr.open_dataset(model_dir / "model_skills" / "deterministic_spatial_skill.nc")
 
 ds_averaged_skill = xr.open_dataset(model_dir / "model_skills" / "deterministic_global_skill.nc")
+
+plot_averaged_skill(ds_averaged_skill, skill="RMSE", variables=["feature"]).savefig(
+        model_dir / "figs" / "skills" / "RMSE_skill.png"
+    )
+
+plot_averaged_skills(ds_averaged_skill, variables=["feature"]).savefig(
+        model_dir / "figs" / "skills" / "averaged_skill.png"
+    )
+plot_skills_distribution(ds_skill, variables=["feature"]).savefig(
+    model_dir / "figs" / "skills" / "skills_distribution.png",
+)

nowproject/utils/config.py
@@ -612,16 +612,16 @@ def create_event_time_range(event_dict, target_timezone="Europe/Zurich", freq="2
                          end=start_time + datetime.timedelta(hours=event_dict["duration"]), 
                          freq=freq).to_numpy().astype("M8[s]")
 
-def create_event_time_range_autoregressive(event_dict, input_k, target_timezone="Europe/Zurich", freq="2min30s"):
+def create_event_time_range_autoregressive(event_dict, nb_ar, target_timezone="Europe/Zurich", freq="2min30s"):
     regular_time_range = create_event_time_range(event_dict, target_timezone=target_timezone, freq=freq)
-    additional_time_range = pd.date_range(end=regular_time_range[0], periods=len(input_k), 
+    additional_time_range = pd.date_range(end=regular_time_range[0], periods=nb_ar + 1, 
                                           freq=freq, closed="left").to_numpy().astype("M8[s]")
     return np.concatenate([additional_time_range, regular_time_range])
 
-def create_test_events_autoregressive_time_range(fpath, input_k, freq="2min30s"):
+def create_test_events_autoregressive_time_range(fpath, nb_ar, freq="2min30s"):
     with open(fpath, "r", encoding="utf-8") as f:
         event_dicts = json.load(f)
-        return [create_event_time_range_autoregressive(d, input_k, freq=freq) for d in event_dicts]
+        return [create_event_time_range_autoregressive(d, nb_ar, freq=freq) for d in event_dicts]
 
 def create_test_events_time_range(fpath, freq="2min30s"):
     with open(fpath, "r", encoding="utf-8") as f:

nowproject/utils/scalers.py
@@ -188,7 +188,7 @@ class RainBinScaler:
         self.scaler_class = "RainBinScaler"
         self.bins = bins
         self.centres = centres
-        self.inverse_bins = np.vectorize(lambda x: centres[x], otypes=[float])
+        self.inverse_bins = np.vectorize(lambda x: centres[x] if x < len(centres) else centres[-1], otypes=[float])
 
     ##------------------------------------------------------------------------.
     def transform(self, new_data, variable_dim=None, rename_dict=None):
@@ -225,7 +225,7 @@ class RainBinScaler:
             var_dims = new_data[var].dims
 
             values = new_data[var].values
-            values = values.clip(max=self.bins[-1])
+            values = values.clip(max=(self.bins[-1] - 0.01))
             values = np.digitize(values, bins=self.bins)
 
             new_data[var] = (var_dims, values)
@@ -284,6 +284,7 @@ class RainBinScaler:
             var_dims = new_data[var].dims
 
             values = new_data[var].values
+            values = np.rint(values).astype(int)
             values = self.inverse_bins(values)
 
             new_data[var] = (var_dims, values)

scripts/02_compute_benchmarks.py
@@ -7,55 +7,60 @@ from pysteps import motion, nowcasts
 from nowproject.utils.config import (
     create_test_events_autoregressive_time_range
 )
+from nowproject.data.data_utils import prepare_data_dynamic
+from xforecasting.predictions_autoregressive import reshape_forecasts_for_verification
+
+LEADTIME = 21
+FREQ_IN_NS = 150000000000
+LEADTIMES = np.arange(FREQ_IN_NS, FREQ_IN_NS*(LEADTIME + 1), FREQ_IN_NS, dtype="timedelta64[ns]")
+AR = [-3, -2, -1]
 
 data_dir_path = Path("/ltenas3/0_Data/NowProject/")
 test_events_path = Path("/home/haddad/nowproject/configs/events.json")
 exp_dir_path = Path("/home/haddad/experiments/")
 
-data_dynamic = xr.open_zarr(data_dir_path / "zarr" / "rzc_temporal_chunk.zarr")
-data_dynamic = data_dynamic.reset_coords(
-    ["radar_names", "radar_quality", "radar_availability"], 
-    drop=True
-    )
-data_dynamic = data_dynamic.sel(time=slice(None, "2021-09-01T00:00"))
-data_dynamic = data_dynamic.sel(
-    {"y": list(range(835, 470, -1)), 
-    "x": list(range(60, 300))}
-    )
-data_dynamic = data_dynamic.rename({"precip": "feature"})[["feature"]]
-
-test_events = create_test_events_autoregressive_time_range(test_events_path, [-3, -2, -1])
+data_dynamic = prepare_data_dynamic(data_dir_path / "zarr" / "rzc_temporal_chunk.zarr")
+test_events = create_test_events_autoregressive_time_range(test_events_path, len(AR))
 
 sprog = []
 steps = []
 for event in test_events:
+    event_sprog = []
+    event_steps = []
     oflow_method = motion.get_method("lucaskanade")
     R = data_dynamic.sel(time=event).feature.values
     V = oflow_method(R)
+    for i in range(len(event) - len(AR)):
+        nowcast_method = nowcasts.get_method("sprog")
+        R_f_sprog = nowcast_method(R[i:i + len(AR) + 1, :, :], V, timesteps=LEADTIME, R_thr=0.1)
+        event_sprog.append(R_f_sprog)
 
-    nowcast_method = nowcasts.get_method("sprog")
-    R_f_sprog = nowcast_method(R[:3, :, :], V, timesteps=len(event[2:]), R_thr=0.1)
-    sprog.append(R_f_sprog)
+        nowcast_method = nowcasts.get_method("steps")
+        R_f_steps = nowcast_method(R[i:i + len(AR) + 1, :, :], V, timesteps=LEADTIME, 
+                                   n_ens_members=24, n_cascade_levels=8, kmperpixel=1.0, 
+                                   R_thr=0.1, timestep=2.5)
 
-    nowcast_method = nowcasts.get_method("steps")
-    R_f_steps = nowcast_method(R[:3, :, :], V, timesteps=len(event[2:]), n_ens_members=24, 
-                            n_cascade_levels=8, kmperpixel=1.0, R_thr=0.1, timestep=2.5)
+        R_f_steps = np.mean(R_f_steps, axis=0)
+        event_steps.append(R_f_steps)
 
-    R_f_steps = np.mean(R_f_steps, axis=0)
-    steps.append(R_f_steps)
+    sprog.append(np.asarray(event_sprog))
+    steps.append(np.asarray(event_steps))
 
 ds_benchmark = xr.Dataset(
     data_vars=dict(
-        sprog=(["time", "y", "x"], np.concatenate(sprog, axis=0)),
-        steps=(["time", "y", "x"], np.concatenate(steps, axis=0)),
+        sprog=(["forecast_reference_time", "leadtime", "y", "x"], np.concatenate(sprog, axis=0)),
+        steps=(["forecast_reference_time", "leadtime", "y", "x"], np.concatenate(steps, axis=0)),
     ),
     coords=dict(
-        time=np.concatenate([event[2:] for event in test_events]),
+        forecast_reference_time=np.concatenate([event[len(AR):] for event in test_events]),
+        leadtime=LEADTIMES,
         x=(["x"], data_dynamic.x.data),
         y=(["y"], data_dynamic.y.data),
     ),
     attrs={},
 )
 
-ds_benchmark.to_zarr(exp_dir_path / "benchmark.zarr")
+ds_benchmark = reshape_forecasts_for_verification(ds_benchmark)
+
+ds_benchmark.to_zarr(exp_dir_path / "benchmark.zarr", mode="w")
 

scripts/train.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 import sys
 import shutil
 import argparse
@@ -51,10 +52,10 @@ from nowproject.utils.plot import (
     plot_skills_distribution
 )
 from nowproject.utils.scalers import RainScaler, RainBinScaler
-from nowproject.data.data_config import METADATA
-from nowproject.data.data_utils import load_static_topo_data
+from nowproject.data.data_config import METADATA, BOTTOM_LEFT_COORDINATES
+from nowproject.data.data_utils import load_static_topo_data, prepare_data_dynamic
 
-def main(cfg_path, data_dir_path, static_data_dir_path, test_events_path, 
+def main(cfg_path, data_dir_path, static_data_path, test_events_path, 
          exp_dir_path, force=False):
     """General function for training models."""
 
@@ -70,26 +71,20 @@ def main(cfg_path, data_dir_path, static_data_dir_path, test_events_path,
 
     ##------------------------------------------------------------------------.
     # Load Zarr Datasets
-    data_dynamic = xr.open_zarr(data_dir_path / "zarr" / "rzc_temporal_chunk.zarr")
-    data_dynamic = data_dynamic.reset_coords(
-        ["radar_names", "radar_quality", "radar_availability"], 
-        drop=True
-        )
-    data_dynamic = data_dynamic.sel(time=slice(None, "2021-09-01T00:00"))
-    # data_dynamic = data_dynamic.sel({"y": list(range(850, 450, -1)), "x": list(range(30, 320))})
-    data_dynamic = data_dynamic.sel(
-        {"y": list(range(835, 470, -1)), 
-        "x": list(range(60, 300))}
-        )
-    data_dynamic = data_dynamic.rename({"precip": "feature"})[["feature"]]
-    data_static = load_static_topo_data(static_data_dir_path, data_dynamic)
+    data_dynamic = prepare_data_dynamic(data_dir_path / "zarr" / "rzc_temporal_chunk.zarr")
+    data_static = load_static_topo_data(static_data_path, data_dynamic)
     data_bc = None
 
     ##------------------------------------------------------------------------.
     # Load scalers
-    scaler = RainScaler(feature_min=np.log10(0.025), 
-                        feature_max=np.log10(150), 
-                        threshold=np.log10(0.1))
+    # scaler = RainScaler(feature_min=np.log10(0.025), 
+    #                     feature_max=np.log10(150), 
+    #                     threshold=np.log10(0.1))
+    
+    bins = [0.0, 0.1, 0.5, 1.0, 2.0, 4.0, 8.0, 12.0, 20.0, 30.0, 50.0, 80.0, 120.0, 250.0]
+
+    centres = [0] + [(bins[i] + bins[i+1])/2 for i in range(0, len(bins)-1)] + [np.nan]
+    scaler = RainBinScaler(bins, centres)
 
 
     ##------------------------------------------------------------------------.
@@ -97,9 +92,9 @@ def main(cfg_path, data_dir_path, static_data_dir_path, test_events_path,
     ## - Defining time split for training
     # training_years = np.array(["2018-05-01T00:00", "2020-12-31T23:57:30"], dtype="M8[s]")
     # validation_years = np.array(["2021-01-01T00:00", "2021-12-31T23:57:30"], dtype="M8[s]")
-    training_years = np.array(["2018-10-01T00:00", "2018-10-10T23:57:30"], dtype="M8[s]")
-    validation_years = np.array(["2021-01-01T00:00", "2021-01-10T23:57:30"], dtype="M8[s]")
-    test_events = create_test_events_time_range(test_events_path)[:1]
+    training_years = np.array(["2018-10-01T00:00", "2018-11-30T23:57:30"], dtype="M8[s]")
+    validation_years = np.array(["2021-01-01T00:00", "2021-01-31T23:57:30"], dtype="M8[s]")
+    test_events = create_test_events_time_range(test_events_path)
 
     # - Split data sets
     t_i = time.time()
@@ -109,8 +104,6 @@ def main(cfg_path, data_dir_path, static_data_dir_path, test_events_path,
     validation_data_dynamic = data_dynamic.sel(
         time=slice(validation_years[0], validation_years[-1])
     )
-    # test_data_dynamic = [data_dynamic.sel(time=event) for event in test_events]
-    # test_data_dynamic = data_dynamic.sel(time=np.concatenate(test_events))
 
     print(
         "- Splitting data into train, validation and test sets: {:.2f}s".format(
@@ -395,8 +388,8 @@ def main(cfg_path, data_dir_path, static_data_dir_path, test_events_path,
     # dask.config.set(scheduler='processes')
     # - Compute skills
     ds_skill = xverif.deterministic(
-        pred=ds_verification_format.load().chunk({"x": 1, "y": 1}),
-        obs=data_dynamic.sel(time=ds_verification_format.time).load().chunk({"x": 1, "y": 1}),
+        pred=ds_verification_format.stack(sample=("x", "y")),
+        obs=data_dynamic.sel(time=ds_verification_format.time).stack(sample=("x", "y")),
         forecast_type="continuous",
         aggregating_dim="time",
     )
@@ -407,24 +400,24 @@ def main(cfg_path, data_dir_path, static_data_dir_path, test_events_path,
     ### - Create verification summary plots and maps
     print("========================================================================================")
     print("- Create verification summary plots and maps")
-    ds_averaged_skill = ds_skill.mean(dim=["y", "x"])
+    ds_averaged_skill = ds_skill.mean(dim=["sample"])
     
     # - Save averaged skills
     ds_averaged_skill.to_netcdf(model_dir / "model_skills" / "deterministic_global_skill.nc")
 
-    # bbox = (451000, 30000, 850000, 319000)
-    bbox = (470000, 60000, 835000, 300000)
-    # - Create spatial maps
-    plot_skill_maps(
-        ds_skill=ds_skill,
-        figs_dir=(model_dir / "figs" / "skills" / "SpatialSkill"),
-        geodata=METADATA,
-        bbox=bbox,
-        skills=["BIAS", "RMSE", "rSD", "pearson_R2", "error_CoV"],
-        variables=["feature"],
-        suffix="",
-        prefix="",
-    )
+    # # bbox = (451000, 30000, 850000, 319000)
+    # bbox = (470000, 60000, 835000, 300000)
+    # # - Create spatial maps
+    # plot_skill_maps(
+    #     ds_skill=ds_skill,
+    #     figs_dir=(model_dir / "figs" / "skills" / "SpatialSkill"),
+    #     geodata=METADATA,
+    #     bbox=bbox,
+    #     skills=["BIAS", "RMSE", "rSD", "pearson_R2", "error_CoV"],
+    #     variables=["feature"],
+    #     suffix="",
+    #     prefix="",
+    # )
 
     # - Create skill vs. leadtime plots
     plot_averaged_skill(ds_averaged_skill, skill="RMSE", variables=["feature"]).savefig(
@@ -449,7 +442,7 @@ def main(cfg_path, data_dir_path, static_data_dir_path, test_events_path,
 
 if __name__ == "__main__":
     default_data_dir = "/ltenas3/0_Data/NowProject/"
-    default_static_data_dir = "/ltenas3/0_GIS/DEM Switzerland/"
+    default_static_data_path = "/ltenas3/0_GIS/DEM Switzerland/srtm_Switzerland_EPSG21781.tif"
     default_exp_dir = "/home/haddad/experiments/"
     default_config = "/home/haddad/nowproject/configs/UNet/AvgPool4-Conv3.json"
     default_test_events = "/home/haddad/nowproject/configs/events.json"
@@ -460,7 +453,7 @@ if __name__ == "__main__":
     parser.add_argument("--config_file", type=str, default=default_config)
     parser.add_argument("--test_events_file", type=str, default=default_test_events)
     parser.add_argument("--data_dir", type=str, default=default_data_dir)
-    parser.add_argument("--static_data_dir", type=str, default=default_static_data_dir)
+    parser.add_argument("--static_data_path", type=str, default=default_static_data_path)
     parser.add_argument("--exp_dir", type=str, default=default_exp_dir)
     parser.add_argument("--cuda", type=str, default="0")
     parser.add_argument("--force", type=str, default="True")
@@ -476,7 +469,7 @@ if __name__ == "__main__":
     main(
         cfg_path=Path(args.config_file),
         exp_dir_path=Path(args.exp_dir),
-        static_data_dir_path=Path(args.static_data_dir),
+        static_data_path=Path(args.static_data_path),
         test_events_path=Path(args.test_events_file),
         data_dir_path=Path(args.data_dir),
         force=force,