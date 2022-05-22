Parallelise the benchmark computation and fix a bug

scripts/02_compute_benchmarks.py
@@ -13,13 +13,15 @@ from xforecasting.predictions_autoregressive import reshape_forecasts_for_verifi
 LEADTIME = 21
 FREQ_IN_NS = 150000000000
 LEADTIMES = np.arange(FREQ_IN_NS, FREQ_IN_NS*(LEADTIME + 1), FREQ_IN_NS, dtype="timedelta64[ns]")
-AR = [-3, -2, -1]
+AR = [-2, -1]
 
 data_dir_path = Path("/ltenas3/0_Data/NowProject/")
 test_events_path = Path("/home/haddad/nowproject/configs/events.json")
 exp_dir_path = Path("/home/haddad/experiments/")
 
-data_dynamic = prepare_data_dynamic(data_dir_path / "zarr" / "rzc_temporal_chunk.zarr")
+boundaries = {"x": slice(485, 831), "y": slice(301, 75)}
+data_dynamic = prepare_data_dynamic(data_dir_path / "zarr" / "rzc_temporal_chunk.zarr",
+                                    boundaries=boundaries)
 test_events = create_test_events_autoregressive_time_range(test_events_path, len(AR))
 
 sprog = []
@@ -32,19 +34,25 @@ for event in test_events:
     V = oflow_method(R)
     for i in range(len(event) - len(AR)):
         nowcast_method = nowcasts.get_method("sprog")
-        R_f_sprog = nowcast_method(R[i:i + len(AR) + 1, :, :], V, timesteps=LEADTIME, R_thr=0.1)
+        R_f_sprog = nowcast_method(R[i:i + len(AR) + 1, :, :], V, timesteps=LEADTIME, 
+                                     R_thr=0.1, num_workers=18, measure_time=True, 
+                                     ar_order=len(AR))
         event_sprog.append(R_f_sprog)
 
         nowcast_method = nowcasts.get_method("steps")
-        R_f_steps = nowcast_method(R[i:i + len(AR) + 1, :, :], V, timesteps=LEADTIME, 
-                                   n_ens_members=24, n_cascade_levels=8, kmperpixel=1.0, 
-                                   R_thr=0.1, timestep=2.5)
+        try:
+            R_f_steps = nowcast_method(R[i:i + len(AR) + 1, :, :], V, timesteps=LEADTIME, 
+                                    n_ens_members=24, n_cascade_levels=8, kmperpixel=1.0, ar_order=len(AR),
+                                    R_thr=0.1, timestep=2.5, num_workers=18, measure_time=True)
+        except:
+            R_f_steps = np.zeros((24, LEADTIME, data_dynamic.y.size, data_dynamic.x.size), dtype=float)
 
         R_f_steps = np.mean(R_f_steps, axis=0)
         event_steps.append(R_f_steps)
 
     sprog.append(np.asarray(event_sprog))
     steps.append(np.asarray(event_steps))
+
 
 ds_benchmark = xr.Dataset(
     data_vars=dict(