Correct benchmark computation

scripts/02_compute_benchmarks.py
@@ -10,6 +10,8 @@ from nowproject.utils.config import (
 from nowproject.data.data_utils import prepare_data_dynamic
 from xforecasting.predictions_autoregressive import reshape_forecasts_for_verification
 
+from pysteps.utils import transformation
+
 LEADTIME = 21
 FREQ_IN_NS = 150000000000
 LEADTIMES = np.arange(FREQ_IN_NS, FREQ_IN_NS*(LEADTIME + 1), FREQ_IN_NS, dtype="timedelta64[ns]")
@@ -23,52 +25,62 @@ boundaries = {"x": slice(485, 831), "y": slice(301, 75)}
 data_dynamic = prepare_data_dynamic(data_dir_path / "zarr" / "rzc_temporal_chunk.zarr",
                                     boundaries=boundaries)
 test_events = create_test_events_autoregressive_time_range(test_events_path, len(AR))
+(exp_dir_path / "benchmarks").mkdir(parents=True, exist_ok=True)
 
-sprog = []
-steps = []
-for event in test_events:
+steps_vel_pert_init = {
+    "p_par": [2.31970635, 0.33734287, -2.64972861],
+    "p_perp": [1.90769947, 0.33446594, -2.06603662]
+}
+
+for idx, event in enumerate(test_events):
     event_sprog = []
     event_steps = []
     oflow_method = motion.get_method("lucaskanade")
     R = data_dynamic.sel(time=event).feature.values
+    R = transformation.dB_transform(R, threshold=0.1, zerovalue=-15.0)[0]
+    R[~np.isfinite(R)] = -15.0
     V = oflow_method(R)
     for i in range(len(event) - len(AR)):
         nowcast_method = nowcasts.get_method("sprog")
-        R_f_sprog = nowcast_method(R[i:i + len(AR) + 1, :, :], V, timesteps=LEADTIME, 
-                                     R_thr=0.1, num_workers=18, measure_time=True, 
-                                     ar_order=len(AR))
+        try:
+            R_f_sprog = nowcast_method(R[i:i + len(AR) + 1, :, :], V, timesteps=LEADTIME, 
+                                        R_thr=-10.0, num_workers=18, measure_time=True, 
+                                        ar_order=len(AR))[0]
+            R_f_sprog = transformation.dB_transform(R_f_sprog, threshold=-10.0, inverse=True)[0]
+        except:
+            R_f_sprog = np.zeros((LEADTIME, data_dynamic.y.size, data_dynamic.x.size), dtype=float)
+        
         event_sprog.append(R_f_sprog)
 
         nowcast_method = nowcasts.get_method("steps")
         try:
             R_f_steps = nowcast_method(R[i:i + len(AR) + 1, :, :], V, timesteps=LEADTIME, 
                                     n_ens_members=24, n_cascade_levels=8, kmperpixel=1.0, ar_order=len(AR),
-                                    R_thr=0.1, timestep=2.5, num_workers=18, measure_time=True)
-        except:
-            R_f_steps = np.zeros((24, LEADTIME, data_dynamic.y.size, data_dynamic.x.size), dtype=float)
+                                    R_thr=-10.0, timestep=2.5, num_workers=18, vel_pert_kwargs=steps_vel_pert_init,
+                                    measure_time=True)
+            if type(R_f_steps) == tuple:
+                R_f_steps = R_f_steps[0]
 
-        R_f_steps = np.mean(R_f_steps, axis=0)
+            R_f_steps = np.mean(R_f_steps, axis=0)
+            R_f_steps = transformation.dB_transform(R_f_steps, threshold=-10.0, inverse=True)[0]
+        except:
+            R_f_steps = np.zeros((LEADTIME, data_dynamic.y.size, data_dynamic.x.size), dtype=float)
+
         event_steps.append(R_f_steps)
 
-    sprog.append(np.asarray(event_sprog))
-    steps.append(np.asarray(event_steps))
+    ds_benchmark = xr.Dataset(
+        data_vars=dict(
+            sprog=(["forecast_reference_time", "leadtime", "y", "x"], np.asarray(event_sprog)),
+            steps=(["forecast_reference_time", "leadtime", "y", "x"], np.asarray(event_steps)),
+        ),
+        coords=dict(
+            forecast_reference_time=event[len(AR):],
+            leadtime=LEADTIMES,
+            x=(["x"], data_dynamic.x.data),
+            y=(["y"], data_dynamic.y.data),
+        ),
+        attrs={},
+    )
 
-
-ds_benchmark = xr.Dataset(
-    data_vars=dict(
-        sprog=(["forecast_reference_time", "leadtime", "y", "x"], np.concatenate(sprog, axis=0)),
-        steps=(["forecast_reference_time", "leadtime", "y", "x"], np.concatenate(steps, axis=0)),
-    ),
-    coords=dict(
-        forecast_reference_time=np.concatenate([event[len(AR):] for event in test_events]),
-        leadtime=LEADTIMES,
-        x=(["x"], data_dynamic.x.data),
-        y=(["y"], data_dynamic.y.data),
-    ),
-    attrs={},
-)
-
-ds_benchmark = reshape_forecasts_for_verification(ds_benchmark)
-
-ds_benchmark.to_zarr(exp_dir_path / "benchmark.zarr", mode="w")
-
+    ds_benchmark = reshape_forecasts_for_verification(ds_benchmark)
+    ds_benchmark.to_zarr(exp_dir_path / "benchmarks" / f"benchmark_test_event_{idx}.zarr", mode="w")